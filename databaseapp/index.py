aa59831b7a707ec1142b7f376367222230d502a7
Baron <baronrustamov@gmail.com>
2021-04-18 16:32:30 +0300

    cmt

--- databaseapp/index.py
+++
@@ -1,9 +0,0 @@
-import httplib, urllib, base64
-
-from django.shortcuts import render
-from django.template import loader
-from django.template import Context
-from django.conf import settings
-from django.core.files.storage import FileSystemStorage
-def mainPage(request):
-	return  render(request, 'databaseapp/index.html')
