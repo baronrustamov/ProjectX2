638e278f752c36669dcb715819fa17b082f4bebf
baronrustamov <baronrustamov@gmail.com>
2021-04-18 16:46:33 +0300

    static

--- databaseapp/database.py
+++ databaseapp/database.py
@@ -7,18 +7,19 @@ from django.db import models
 from django.conf import settings
 from .models import Department as de
 from .models import Student as stu
+
 def addDepartment(request):
     if request.method == 'GET':
-    	try:
-	        did  = request.GET.get('did','')
-	        dname=request.GET.get('dname','')
+		try:
+			did  = request.GET.get('did','')
+			dname=request.GET.get('dname','')
 	        #d=de.objects.create(dept_id=did,dept_name=dname)
 	        d=de(dept_id=did,dept_name=dname)
 	        d.save()
-	        #st=stu(student_name=did,student_usn=dname,sem=7,dept_id_id="cve")
+			#st=stu(student_name=did,student_usn=dname,sem=7,dept_id_id="cve")
 	        #st.save()
-        except:
-			print('smthng gone wrong')
+        except
+			pass
 
 	#print("[Errno {0}] {1}".format(e.errno, e.strerror))
     #return data
